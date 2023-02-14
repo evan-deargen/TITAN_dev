ff7fcea123d72dee5984cfaebc4380ee5887ee2e
Evan <evan.dergen@gmail.com>
2023-02-14 15:53:28 +0900

    [REFACTOR] Code refactoring. Moving directories. Add files

--- source/models/__init__.py
+++ source/models/__init__.py
@@ -1,6 +1,8 @@
 from .knn import knn
+from .bimodal_mca import BimodalMCA
 
 # More models could follow
 MODEL_FACTORY = {
+    'bimodal_mca': BimodalMCA,
     'knn': knn
 }
---
+++ source/utils/test.py
@@ -0,0 +1,5 @@
+"""Custom layers implementation."""
+from collections import OrderedDict
+
+from .utils import Squeeze, get_device, Temperature, Unsqueeze
+
